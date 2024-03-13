6a06305cf80a866bb241296b46db26c599059dc6
Saverio Miroddi <saverio.pub2@gmail.com>
2024-03-13 11:32:43 +0100

    add_new_article.sh: Skip README included in the tags directory

--- _utils/add_new_article.sh
+++ _utils/add_new_article.sh
@@ -36,7 +36,7 @@ Creates the branch, and the article file.
 c_posts_path=$(dirname "$0")/../_posts
 c_tags_path=$(dirname "$0")/../_tags
 c_branch_prefix=add_article_
-
+c_tags_readme_prefix=README
 c_editor_program=code
 
 ################################################################################
@@ -83,6 +83,7 @@ function prepare_article_filename {
 function find_tags {
   find "$c_tags_path" -type f -printf '%P\n' \
     | sed 's/\.md$//' \
+    | grep -v "^$c_tags_readme_prefix" \
     | sort
 }
 
